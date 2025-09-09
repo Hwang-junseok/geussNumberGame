b98445f45841da0423be4bef818684e0e32eec9b
Junseok Hwang <sunday6003@gmail.com>
2025-09-09 12:41:08 +0900

    third commit

--- main.js
+++ main.js
@@ -16,9 +16,14 @@ let resetButton = document.getElementById("reset-button")
 let chances = 5
 let gameOver = false;
 let chanceArea = document.getElementById("chance-area")
+let history = [];
 
 playButton.addEventListener("click", play)
 resetButton.addEventListener("click",reset)
+userInput.addEventListener("focus", function(){
+    userInput.value = "";
+});
+
 function pickRandomNum(){
     computerNum = Math.floor(Math.random()*100)+1;
     console.log("정답", computerNum);
@@ -26,7 +31,16 @@ function pickRandomNum(){
 
 function play(){
     let userValue = userInput.value;
+    
+    if(userValue < 1 || userValue > 100){
+        resultArea.textContent = "1과 100 사이 숫자를 입력해 주세요"
+        return;
+    }
 
+    if(history.includes(userValue)){
+        resultArea.textContent = "이미 입력한 숫자입니다 다른 숫자를 입력해 주세요"
+        return
+    }
     chances--;
     chanceArea.textContent = `남은 기회:${chances}번`;
     console.log
@@ -38,7 +52,11 @@ function play(){
         resultArea.textContent = "DOWN!!!"
     }else{
         resultArea.textContent = "맞추셨습니다!!!"
+        gameOver = true
     }
+
+    history.push(userValue)
+    console.log(history)
     if(chances < 1){
         gameOver = true;
     }
